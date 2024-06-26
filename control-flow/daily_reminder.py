f05ecc670fde281ea3fa1439544109cbb855c37c
Guandaru-Kabugi <kabugi96@gmail.com>
2024-06-26 13:45:16 +0300

    modifications

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,8 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
+reminder = None
+note = None
 match priority:
     case "high": reminder = f"Reminder: "
     case "low" : note = f"Note: "
