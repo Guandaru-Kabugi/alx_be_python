2c27bf2af8e1d5b8bf30e9f14743112788fd70d5
Guandaru-Kabugi <kabugi96@gmail.com>
2024-07-05 11:36:40 +0300

    first push of temperature conversion tool

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,18 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = 0
+CELSIUS_TO_FAHRENHEIT_FACTOR = 0
+
+def convert_to_celsius(fahrenheit):
+    FAHRENHEIT_TO_CELSIUS_FACTOR = (fahrenheit - 32) / 1.8
+    print(f"{fahrenheit}°F is {FAHRENHEIT_TO_CELSIUS_FACTOR}°C")
+def convert_to_fahrenheit(celsius):
+    CELSIUS_TO_FAHRENHEIT_FACTOR = 32 + (1.8 * celsius)
+    print(f"{celsius}°C is {CELSIUS_TO_FAHRENHEIT_FACTOR}°F")
+temperature_to_convert = float(input("Enter the temperature to convert: "))
+format_of_temperature = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
+match format_of_temperature:
+    case 'c':
+        convert_to_fahrenheit(temperature_to_convert)
+    case 'f':
+        convert_to_celsius(temperature_to_convert)
+    case _:
+        print("Invalid temperature. Please enter a numeric value.")
