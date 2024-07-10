6b0d10d7c9c5d6f70c00cfedfad184da6c8cb1aa
Guandaru-Kabugi <kabugi96@gmail.com>
2024-07-10 13:07:43 +0300

    Fourth push

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,9 +1,26 @@
+# def safe_divide(numerator, denominator):
+#     try:
+#         numerator = float(numerator)
+#         denominator = float(denominator)
+#         if not  isinstance (numerator,(int, float)) or not isinstance (denominator,(int,float)):
+#             raise ValueError("Error: Please enter numeric values only.")
+            
+#         if numerator == 0 or denominator == 0:
+#             raise ZeroDivisionError("Error: Cannot divide by zero.")
+#         else:
+#             results = (numerator)/(denominator)
+#             return (f"The result of the division is {results}")
+#     except ValueError:
+#         return "Error: Please enter numeric values only."
+#     except (ZeroDivisionError,ValueError) as e:
+#         return e
+
 def safe_divide(numerator, denominator):
     try:
         numerator = float(numerator)
         denominator = float(denominator)
         if not  isinstance (numerator,(int, float)) or not isinstance (denominator,(int,float)):
-            raise ValueError("Error: Please enter numeric values only.")
+            raise ValueError
             
         if numerator == 0 or denominator == 0:
             raise ZeroDivisionError("Error: Cannot divide by zero.")
