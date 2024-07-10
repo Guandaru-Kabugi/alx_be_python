9e916faf90812b65c869ba566d09c41ac461cfad
Guandaru-Kabugi <kabugi96@gmail.com>
2024-07-10 11:57:06 +0300

    task 0

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,12 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+    def deposit(self, amount):
+        self.account_balance+=amount
+        return amount
+    def withdraw(self, amount):
+        if amount <=self.account_balance:
+            return True
+        return False
+    def display_balance(self):
+        print(f"Your current account balance is {self.account_balance}.")
