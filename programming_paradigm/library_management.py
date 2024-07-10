fc99dcde48cbbc8b6b5e8b269b17656734d5e35a
Guandaru-Kabugi <kabugi96@gmail.com>
2024-07-10 15:39:50 +0300

    First push

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,29 @@
+class Book:
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+        
+class Library():
+    
+    def __init__(self):
+        self._books = []
+        self._checked_out_books = []
+    def add_book(self, book):
+        self._books.append(book)
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title and not book._is_checked_out:
+                book._is_checked_out = True
+                self._books.remove(book)
+                self._checked_out_books.append(book)
+                
+    def return_book(self, title):
+        for book in self._checked_out_books:
+            if book.title == title and book._is_checked_out:
+                book._is_checked_out = False
+                self._checked_out_books.remove(book)            
+                self._books.append(book)
+    def list_available_books(self):
+        for book in self._books:
+            print(f"{book.title} by {book.author}")
