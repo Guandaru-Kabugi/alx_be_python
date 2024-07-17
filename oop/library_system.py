daa292691a645c9df91710244f1623f8dc72ed17
Guandaru-Kabugi <kabugi96@gmail.com>
2024-07-17 13:51:51 +0300

    advanced oop

---
+++ oop/library_system.py
@@ -0,0 +1,29 @@
+class Book:
+    def __init__(self, title:str, author:str):
+        self.title = title
+        self.author = author
+    def __repr__(self) -> str:
+        return f"Book: {self.title} by {self.author}"
+    
+class EBook(Book):
+    def __init__(self, title: str, author: str, file_size:int):
+        super().__init__(title, author)
+        self.filesize = file_size
+    def __repr__(self) -> str:
+        return f"EBook: {self.title} by {self.author}, Page Count: {self.filesize}KB"
+class PrintBook(Book):
+    def __init__(self, title: str, author: str, page_count:int):
+        super().__init__(title, author)
+        self.pagecount = page_count
+    def __repr__(self) -> str:
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.pagecount}"
+class Library:
+    def __init__(self):
+        self.books = []
+    def add_book(self, book):
+        self.books.append(book)
+    def list_books(self):
+        for book in self.books:
+            print(book)
+       
+            
